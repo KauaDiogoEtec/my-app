2c4a1e476de7830351917513ceaa5fe3d99f5908
KauaDiogoEtec <160239871+KauaDiogoEtec@users.noreply.github.com>
2024-02-27 15:24:31 -0300

    aula 27/02
    oi ewerton :D

---
+++ components/exemplo_0/index.js
@@ -0,0 +1,11 @@
+import { View, Text } from 'react-native';
+
+function Exemplo_0() {
+    return(
+        <View>
+            <Text> Minha primeira tela :D </Text>
+        </View>
+    );
+}
+
+export default Exemplo_0();
