2a18c0921376964c234e656c04ea49ce1f4a3c8e
KauaDiogoEtec <160239871+KauaDiogoEtec@users.noreply.github.com>
2024-03-12 15:23:16 -0300

    12-03
    oi ewerton :D

--- App.js
+++ App.js
@@ -1,13 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
-import Exemplo_0 from './components/exemplo_1';
+import Atividade1 from './components/atividade_1';
 
 export default function App() {
   return (
     <View style={styles.container}>
 
-      <Exemplo_0 />
+      <Atividade1 />
       <Text>Open up App.js to start working on your app!</Text>
 
       <StatusBar style="auto" />
---
+++ components/atividade_1/styles.js
@@ -0,0 +1,19 @@
+import {StyleSheet} from 'react-native';
+
+const styles = StyleSheet.create({
+    container:{
+        backgroundColor: 'gray',
+        padding: 10,
+    },
+    texto1:{
+        color: 'blue',
+        fontSize: 48,
+    },
+    texto2:{
+        color: 'red',
+        fontSize: 48,
+    },
+    
+});
+
+export default styles;
