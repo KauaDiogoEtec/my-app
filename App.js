30612b51be3b27e49559c416b0ba1cdcca853e92
KauaDiogoEtec <160239871+KauaDiogoEtec@users.noreply.github.com>
2024-06-18 15:26:05 -0300

    aula 18-06
    oi ewerton :D

--- App.js
+++ App.js
@@ -17,7 +17,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#069',
+    backgroundColor: '#D1C4E9',
     alignItems: 'center',
     justifyContent: 'center',
   },
